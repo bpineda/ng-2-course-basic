08df50079109ff2598b41505ce744d1991b39f01
bpineda <_@bernardopineda.mx>
2017-04-23 18:27:49 -0500

    Correctly moved the header and footer code to the app component HTML file

--- 05-forms/forms-with-cli/src/app/app-routing.module.ts
+++ 05-forms/forms-with-cli/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 const appRoutes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'template-driven-form',
+    component: TemplateDrivenFormComponent
   },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
