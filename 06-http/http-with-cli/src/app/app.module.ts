7b91fb1f328aa87cd20fa096238ce9a3db5e9531
bpineda <_@bernardopineda.mx>
2017-04-27 00:19:06 -0500

    Removed unused components

--- 06-http/http-with-cli/src/app/app.module.ts
+++ 06-http/http-with-cli/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { GithubService } from './github/shared/github.service';
 import { AppRoutingModule } from './app-routing.module';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
-import { TemplateDrivenFormObjectComponent } from './template-driven-form-object/template-driven-form-object.component';
-import { ModelDrivenFormComponent } from './model-driven-form/model-driven-form.component';
 import { HomeComponent } from './home/home.component';
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
@@ -19,9 +16,6 @@ import { UserListComponent } from './github/user-list/user-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateDrivenFormComponent,
-    TemplateDrivenFormObjectComponent,
-    ModelDrivenFormComponent,
     HomeComponent,
     UserProfileComponent,
     UserListComponent
